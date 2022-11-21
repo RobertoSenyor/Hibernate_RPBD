80b204af2882caf55bbca1ba7c1cbad40227aab3
Roberto <senyor.it@yandex.ru>
2022-11-21 11:48:13 +0700

    add files

---
+++ src/main/java/DAO/Client/ClientDAOImpl.java
@@ -0,0 +1,69 @@
+package DAO.Client;
+
+import models.Client.Client;
+import models.Home.Home;
+
+import java.util.List;
+
+public class ClientDAOImpl implements ClientDAO
+{
+    @Override
+    public Client findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findBySurname(String surname) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findByFathername(String fathername) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findBySerial_of_pasport(int serial) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findByNumber_of_pasport(int number) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findByTelephone(String telephone) {
+        return null;
+    }
+
+    @Override
+    public void save(Client client) {
+
+    }
+
+    @Override
+    public void update(Client client) {
+
+    }
+
+    @Override
+    public void delete(Client client) {
+
+    }
+
+    @Override
+    public Home findHomeById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return null;
+    }
+}
